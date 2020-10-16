92b9e63e9f72e2b91df9b44802525bc9afdd5bc5
Yuri Gaivoronsky <yuri.gaivoronsky@mytona.com>
2020-10-16 15:39:51 +0300

    m3-t2 completed

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -8,6 +8,7 @@ const PRIZES_ID = 2;
 const RULES_ID = 3;
 
 const P1_ICON_PATH = "img/prize1.svg";
+const P2_ICON_PATH = "img/prize2.svg";
 
 export default class FullPageScroll {
   constructor() {
@@ -71,6 +72,7 @@ export default class FullPageScroll {
   changeVisibilityDisplay() {
     let rulesBtn;
     let p1Icon;
+    let p2Icon;
 
     this.screenElements.forEach((screen) => {
       screen.classList.add(`screen--hidden`);
@@ -92,9 +94,14 @@ export default class FullPageScroll {
 
     if (this.activeScreen === PRIZES_ID) {
       p1Icon = document.querySelector('.prizes__item--journeys img');
+      p2Icon = document.querySelector('.prizes__item--cases img');
 
       if (p1Icon.src !== P1_ICON_PATH) {
         p1Icon.src = P1_ICON_PATH;
+      }
+
+      if (p2Icon.src !== P2_ICON_PATH) {
+        p2Icon.src = P2_ICON_PATH;
       }
     }
 
